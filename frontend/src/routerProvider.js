ac88145919c421ba9a4156513f00e10639f87b99
abdias baldiviezo <abadias618@gmail.com>
2022-10-02 16:33:54 -0600

    homepage start

---
+++ frontend/src/routerProvider.js
@@ -0,0 +1,15 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route
+} from "react-router-dom";
+
+import Home from './Home'
+
+const router = createBrowserRouter(
+createRoutesFromElements(
+    <Route path="/" element={<Home />} />
+)
+);
+
+export default router;
